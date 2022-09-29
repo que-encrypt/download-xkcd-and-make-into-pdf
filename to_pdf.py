466bf374dba8b9f69b32bdf9c2deb9515a2db68d
que-encrypt <113485882+que-encrypt@users.noreply.github.com>
2022-09-29 00:06:34 -0700

    added everything

---
+++ to_pdf.py
@@ -0,0 +1,17 @@
+from os import listdir
+from fpdf import FPDF
+
+path = ".//xkcd//" # get the path of images
+
+imagelist = listdir(path) # get list of all images
+
+pdf = FPDF('P','mm','A4') # create an A4-size pdf document 
+
+x,y,w,h = 0,0,200,250
+
+for image in imagelist:
+
+    pdf.add_page()
+    pdf.image(path+image,x,y,w,h)
+
+pdf.output("images.pdf","F")
